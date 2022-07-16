962ce683a5fa94ee8121da9363d09a611a059730
Henrry Mojica <hleomojica@gmail.com>
2022-07-16 17:24:43 +1000

    deploy adjust

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:hleomojica/v2-henrrymojica.me.git master:gh-pages
+git push -f https://hleomojica@github.com/hleomojica/v2-henrrymojica.me.git master:gh-pages
 cd -
